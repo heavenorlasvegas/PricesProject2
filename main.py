e82374e9495989119b9fddbb5f8a57d080f9f6ac
Adam Smith <test@nes.ru>
2022-06-12 20:26:44 +0300

    updates

--- main.py
+++ main.py
@@ -50,9 +50,13 @@ def scrape_prices(city, ingredient, category=""):
 
     options = webdriver.ChromeOptions()
     options.headless = True
-    options.add_argument("--start-maximized")
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_experimental_option('useAutomationExtension', False)
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-features=NetworkService")
+    options.add_argument("--window-size=1920x1080")
+    options.add_argument("--disable-features=VizDisplayCompositor")
     driver = uc.Chrome(options=options)
 
     #driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
