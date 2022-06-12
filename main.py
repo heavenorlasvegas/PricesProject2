4c67dadae19f312817dbef7300612553fd800560
Adam Smith <test@nes.ru>
2022-06-12 19:50:48 +0300

    updates

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def scrape_prices(city, ingredient, category=""):
     options = Options()
     #options.add_argument("--headless")
     options.add_argument("--no-sandbox")
-    #options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-dev-shm-usage")
     #options.add_argument("--disable-gpu")
     #options.add_argument("--disable-features=NetworkService")
     #options.add_argument("--window-size=1920x1080")
